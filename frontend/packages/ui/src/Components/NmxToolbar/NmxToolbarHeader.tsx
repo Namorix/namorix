8d0f913df602087847381a88bb4ea9c04cd0c41e
Nguyen Danh Nam <izero.cs@gmail.com>
2026-08-15 15:01:08 +0700

    feat(ui): NmxToolbarHeader title/icon/onBack props + back action slot

--- frontend/packages/ui/src/Components/NmxToolbar/NmxToolbarHeader.tsx
+++ frontend/packages/ui/src/Components/NmxToolbar/NmxToolbarHeader.tsx
@@ -1,10 +1,18 @@
 import React from "react"
 import type { WithBaseProps } from "../../types"
 import { cx } from "../../utils"
+import {NmxIconFont, type NmxIconFontSymbol} from "../../Primitives"
 
-export type NmxToolbarHeaderProps = WithBaseProps
+export interface NmxToolbarHeaderProps extends WithBaseProps {
+  title?: string
+  icon?: NmxIconFontSymbol
+  onBack?: () => void
+}
 
 export const NmxToolbarHeader: React.FC<NmxToolbarHeaderProps> = ({
+  title,
+  icon,
+  onBack,
   shouldRender = true,
   className,
   children,
@@ -14,6 +22,12 @@ export const NmxToolbarHeader: React.FC<NmxToolbarHeaderProps> = ({
 
   return (
     <div {...rest} className={cx("nmx-toolbar-header", className)}>
+      {(title || icon) && (
+        <div className="nmx-toolbar-header__action-back">
+          {icon && <NmxIconFont symbol={icon}/>}
+          {title}
+        </div>
+      )}
       {children}
     </div>
   )
